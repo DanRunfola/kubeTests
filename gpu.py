64dd60f3d379483e10d89f4b7ed790e30b026e4a
DanRunfola <dan@danrunfola.com>
2023-11-10 23:01:25 -0500

    Path

--- gpu.py
+++ gpu.py
@@ -37,7 +37,7 @@ for device in devs:
 
     # Load CIFAR-10 dataset
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root='/kube/data', train=True, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
     # Train the model
