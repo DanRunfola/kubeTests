3ffa2c4ca8e4139449fa08559619639382a5b3a5
DanRunfola <dan@danrunfola.com>
2023-11-21 06:19:47 -0500

    Distribution

---
+++ dist_gpu.py
@@ -0,0 +1,73 @@
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import time
+
+dist.init_process_group(backend='nccl', init_method='env://')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Define the CNN model
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net().to(device)
+net = torch.nn.parallel.DistributedDataParallel(net)
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# Load CIFAR-10 dataset
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+trainset = torchvision.datasets.CIFAR10(root='/kube/data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+# Train the model
+start_time = time.time()
+
+for epoch in range(1):  # Loop over the dataset multiple times
+    epoch_start_time = time.time()
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()  # Zero the parameter gradients
+        outputs = net(inputs)  # Forward
+        loss = criterion(outputs, labels)
+        loss.backward()  # Backward
+        optimizer.step()  # Optimize
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # Print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    epoch_end_time = time.time()
+    print("Epoch %d completed in %s seconds" % (epoch+1, round(epoch_end_time - epoch_start_time, 2)))
+
+end_time = time.time()
+total_time = end_time - start_time
+print("Trained on: " + str(device))
+print("Total training time: %s seconds" % round(total_time, 2))
+print("----------------------------------")
+print("----------------------------------")
+   
